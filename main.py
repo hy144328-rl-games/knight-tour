3e886c69255ca0f3ed17b529c49b5c1421a6e204
Hans Yu <hans.yu@outlook.de>
2022-01-05 22:30:51 +0100

    Implement valid_moves.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+import itertools
 import numpy as np
+import typing
 
 class Board:
+    possible_moves = {
+        (e[1] * e[0], e[2] * (3 - e[0]))
+        for e in itertools.product(
+            {1, 2},
+            {1, -1},
+            {1, -1},
+        )
+    }
+
     def __init__(
         self,
         no_rows: int=8,
@@ -16,7 +27,7 @@ class Board:
 
         self.current: tuple[int, int] = None
         if first:
-            self.set(first)
+            self.set_knight(first)
 
         self.no_pad: int = 1
         self.col_sep: str = "|"
@@ -26,20 +37,6 @@ class Board:
             + 2 * self.no_pad * no_cols * "-"
             + "\n"
         )
-
-    def __getitem__(self, idx: tuple[int, int]) -> bool:
-        return self.fields[idx]
-
-    def __setitem__(self, idx: tuple[int, int], val: bool):
-        assert isinstance(val, bool)
-        self.fields[idx] = val
-
-    def get(self, idx: tuple[int, int]) -> bool:
-        return self[idx]
-
-    def set(self, idx: tuple[int, int]):
-        self[idx] = True
-        self.current = idx
 
     def __repr__(self) -> str:
         return "".join(
@@ -71,7 +68,41 @@ class Board:
 
         return res
 
+    def __getitem__(self, idx: tuple[int, int]) -> bool:
+        return self.fields[idx]
+
+    def __setitem__(self, idx: tuple[int, int], val: bool):
+        assert isinstance(val, bool)
+        self.fields[idx] = val
+
+    def set_knight(self, idx: tuple[int, int]):
+        self[idx] = True
+        self.current = idx
+
+    def add(self, move: tuple[int, int]) -> tuple[int, int]:
+        return (self.current[0] + move[0], self.current[1] + move[1])
+
+    def is_valid(self, move: tuple[int, int]) -> bool:
+        next_position = self.add(move)
+
+        if next_position[0] < 0 or next_position[0] >= self.no_rows:
+            return False
+
+        if next_position[1] < 0 or next_position[1] >= self.no_cols:
+            return False
+
+        return not self[next_position]
+
+    @property
+    def valid_moves(self) -> set[tuple[int, int]]:
+        return {
+            move_it
+            for move_it in Board.possible_moves
+            if self.is_valid(move_it)
+        }
+
 if __name__ == "__main__":
-    board = Board()
+    board = Board(first=(0, 0))
     print(board)
+    print(board.valid_moves)
 
